36c443011fa91b84c5b3ce34889474f0670de475
joshp423 <205880399+joshp423@users.noreply.github.com>
2025-08-28 17:08:54 +1200

    Added button EL functions for operators and #'s

--- calculator.js
+++ calculator.js
@@ -1,34 +1,65 @@
-let firstNumber;
-let secondNumber;
+let firstNumber = "";
+let secondNumber = "";
 let equationStage = 0;
+let operator;
 
 const display = document.getElementById('displayBox');
-const numbers = document.querySelectorAll(".numbers")
+
+const numbers = document.querySelectorAll(".numbers");
 numbers.forEach((button) => {
     button.addEventListener('click', (event) => {
         let numberSelected = event.target.innerText
-        console.log(numberSelected)
-        numberSet(numberSelected, firstNumber, secondNumber, equationStage, display);
+        if (!firstNumber) {
+            equationStage = 1;
+            firstNumber = numberSet(numberSelected, firstNumber, secondNumber, equationStage);
+            console.log(firstNumber, equationStage)
+            display.innerText = firstNumber;
+            return;
+        }
+        if (equationStage === 1) {
+            firstNumber = numberSet(numberSelected, firstNumber, secondNumber, equationStage);
+            console.log(firstNumber, equationStage)
+            display.innerText = firstNumber;
+            return;
+        }
+        if (!secondNumber && equationStage ===2) {
+            equationStage = 3;
+        }
+        secondNumber = numberSet(numberSelected, firstNumber, secondNumber, equationStage);
+        console.log(secondNumber, equationStage)
+        if (equationStage === 3) {
+            display.innerText = secondNumber;
+            return;
+        }
+        
     });
 });
 
-function numberSet(numberSelected, firstNumber, secondNumber, equationStage, display) {
+const operators = document.querySelectorAll('.operatorButtons');
+operators.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        operator = event.target.innerText;
+        equationStage = 2;
+        console.log(operator, equationStage)
+    });
+});
+
+function numberSet(numberSelected, firstNumber, secondNumber, equationStage) {
     if (!firstNumber) {
-        firstNumber = String(numberSelected);
-        equationStage = 1;
-        display.innerText = firstNumber
-        console.log(firstNumber, equationStage)
-        return;
+        firstNumber = numberSelected;
+        return firstNumber;
     }
     else if (firstNumber && equationStage === 1) {
-        firstNumber = firstNumber.concat(numberSelected);
-        console.log(firstNumber)
-        return;
+        newNumber = firstNumber + numberSelected;
+        return newNumber;
     }
-    if (secondNumber === null && equationStage > 1) {
+    if (!secondNumber && equationStage === 3 ) {
         secondNumber = numberSelected;
-        console.log(secondNumber)
-        return;
+        return secondNumber;
+    }
+    else if (secondNumber && equationStage === 3) {
+        newNumber = secondNumber + numberSelected;
+        return newNumber;
     }
 }
 
